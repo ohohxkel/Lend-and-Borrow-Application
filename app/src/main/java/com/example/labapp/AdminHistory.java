34aa4f5582407a105c644a8527f83320455b7376
ohohxkel <mikeecantara@gmail.com>
2020-03-09 09:52:41 +0800

    Update history window || functional
    Displaying documentID of a subcollection "Borrow" as the transaction number; documents field as textViews.

--- app/src/main/java/com/example/labapp/AdminHistory.java
+++ app/src/main/java/com/example/labapp/AdminHistory.java
@@ -4,12 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -37,8 +39,10 @@ public class AdminHistory extends AppCompatActivity{
 
     private FirebaseFirestore fStore;
     private TextView textViewTransactionNumber, textViewReturned, textViewDate;
+    private CheckBox checkBoxReturned;
     private ArrayList<UserBorrow> mUserBorrow = new ArrayList<>();
     public static final String TAG = "AdminHistory";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,11 +51,19 @@ public class AdminHistory extends AppCompatActivity{
 
         fStore = FirebaseFirestore.getInstance();
         textViewTransactionNumber = (TextView)  findViewById(R.id.textViewTransactionNumber);
+<<<<<<< Updated upstream
         textViewReturned = (TextView) findViewById(R.id.textViewReturned);
+=======
+>>>>>>> Stashed changes
         textViewDate = findViewById(R.id.textViewDate);
+        checkBoxReturned = findViewById(R.id.check_returned);
 
+<<<<<<< Updated upstream
         textViewReturned.setText("");
+=======
+>>>>>>> Stashed changes
         textViewDate.setText("");
+        textViewTransactionNumber.setText("");
 
         Spinner spinner = findViewById(R.id.spinner_slot1);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.itemsAdd, R.layout.color_spinner_layout);
@@ -72,34 +84,55 @@ public class AdminHistory extends AppCompatActivity{
     }
 
     private void loadTransactions(){
-        CollectionReference users = fStore.collection("users");
         fStore.collectionGroup("Borrow")
-                .whereEqualTo("returned", "false")
+                .whereArrayContains("items", "extn-01")
                 //.orderBy("borrowedDate", Direction.ASCENDING)
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                             if(task.isSuccessful()){
+<<<<<<< Updated upstream
                                 Log.d(TAG, task.getResult().toString());
                                 for (QueryDocumentSnapshot document : task.getResult()) {
                                   Log.d(TAG, document.getId() + " => " + document.getData());
+=======
+                                if (task.getResult() == null){ Log.d(TAG, "NULL QUERY");}
+                                if (task.getResult() != null){ Log.d(TAG, "QUERY HAS SOMETHING     " + task.getResult().size());}
 
-                                  UserBorrow userBorrow = document.toObject(UserBorrow.class);
+                                for (QueryDocumentSnapshot documents : task.getResult()) {
+                                  Log.d(TAG, documents.getId() + " => " + documents.getData());
+>>>>>>> Stashed changes
+
+                                  UserBorrow userBorrow = documents.toObject(UserBorrow.class);
                                   mUserBorrow.add(userBorrow);
 
+                                  if(userBorrow != null) { Log.d(TAG, "userBorrow has something!");}
+                                  if(userBorrow == null) { Log.d(TAG, "userBorrow has something!");}
 
+
+<<<<<<< Updated upstream
+=======
+
+                                  String docID = documents.getId();
+
+>>>>>>> Stashed changes
                                   Timestamp borrowedDate = userBorrow.getBorrowedDate();
                                     Date bDate = borrowedDate.toDate();
                                     String borrowedDateString = bDate.toString();
 
                                   boolean returned = userBorrow.isReturned();
-                                    String returnedString = Boolean.toString(returned);
 
+<<<<<<< Updated upstream
 
 
                                   textViewReturned.setText(returnedString);
+=======
+>>>>>>> Stashed changes
                                   textViewDate.setText(borrowedDateString);
+                                  textViewTransactionNumber.setText(docID);
+
+                                  checkBoxReturned.setChecked(returned);
 
 
                                }
