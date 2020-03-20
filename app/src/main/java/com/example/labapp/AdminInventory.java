971fc5d67ec62c0f61004e75f28a64e70ffb78b5
GeneRoca <eugeneroess@gmail.com>
2020-03-21 04:38:23 +0800

    Floating Add Button

--- app/src/main/java/com/example/labapp/AdminInventory.java
+++ app/src/main/java/com/example/labapp/AdminInventory.java
@@ -1,93 +1,85 @@
 package com.example.labapp;
 
-
-import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import com.github.clans.fab.FloatingActionButton;
 
 public class AdminInventory extends AppCompatActivity {
-    Button button_back, button_load;
-    TextView itemsAvailable;
 
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-    CollectionReference itemsRef = db.collection("Inventory");
-
+    FloatingActionButton fab_category, fab_item;
+    String category_text_input, item_text_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_inventory);
 
-        itemsAvailable = (TextView) findViewById(R.id.itemsAvailable);
+        fab_category=findViewById(R.id.fab_category);
+        fab_category.setOnClickListener(new View.OnClickListener(){
 
-        displayItems();
-
-        button_back = findViewById(R.id.button_back);
-        button_back.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(AdminInventory.this, AdminHome.class);
-                startActivity(intent);
-            }
-        });
+            public void onClick (View view){
+                AlertDialog.Builder enterCategoryName = new AlertDialog.Builder(AdminInventory.this);
+                enterCategoryName.setTitle("Enter item category");
 
+                final EditText category_text = new EditText(AdminInventory.this);
+                category_text.setInputType(InputType.TYPE_CLASS_TEXT);
+                enterCategoryName.setView(category_text);
 
-        button_load = findViewById(R.id.button_load);
-        button_load.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(AdminInventory.this, AdminInventory.class);
-                startActivity(intent);
-            }
-        });
-
-    }
-
-
-    //display items available from the Inventory (firestore)
-    public void displayItems() {
-        itemsRef.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-            @Override
-            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                String data = "";
-
-                for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
-                    User user = documentSnapshot.toObject(User.class);
-                    for (String tag : user.getTags()) {
-                        data += "\n-" + tag;
+                enterCategoryName.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        category_text_input=category_text.getText().toString();
+                        Toast.makeText(AdminInventory.this, ""+category_text_input+" has been added", Toast.LENGTH_LONG).show();
                     }
-                }
-                itemsAvailable.setText(data);
+                });
+
+                enterCategoryName.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                enterCategoryName.show();
+            }
+        });
+        fab_item=findViewById(R.id.fab_item);
+        fab_item.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick (View view){
+                AlertDialog.Builder enterItemName = new AlertDialog.Builder(AdminInventory.this);
+                enterItemName.setTitle("Enter item to add");
+
+                final EditText item_text = new EditText(AdminInventory.this);
+                item_text.setInputType(InputType.TYPE_CLASS_TEXT);
+                enterItemName.setView(item_text);
+
+                enterItemName.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        item_text_input=item_text.getText().toString();
+                        Toast.makeText(AdminInventory.this, ""+item_text_input+" has been added", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                enterItemName.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                enterItemName.show();
             }
         });
     }
 }
-
-
-
-
-
-
-
-
-
-
